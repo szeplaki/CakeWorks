3130d418a4f55f07a7039a64a491ea8881a4c8d3
Szeplaki <szeplaki.eva1@gmail.com>
2022-09-27 21:02:11 +0200

    add basic board style

--- static/js/view/htmlFactory.js
+++ static/js/view/htmlFactory.js
@@ -22,8 +22,34 @@ export function htmlFactory(template) {
 
 function boardBuilder(board) {
     return `<div class="board-container">
-                <div class="board" data-board-id=${board.id}>${board.title}</div>
-                <button class="toggle-board-button" data-board-id="${board.id}">Show Cards</button>
+                <section class="board">
+            <div class="board-header"><span class="board-title" data-board-id=${board.id}>${board.title}</span>
+                <button class="board-add">Add Card</button>
+                <button class="board-toggle" data-board-id="${board.id}"><i class="fas fa-chevron-down"></i></button>
+            </div>
+            <div class="board-columns">
+                <div class="board-column">
+                    <div class="board-column-title">New</div>
+                    <div class="board-column-content">
+                    </div>
+                </div>
+                <div class="board-column">
+                    <div class="board-column-title">In Progress</div>
+                    <div class="board-column-content">
+                    </div>
+                </div>
+                <div class="board-column">
+                    <div class="board-column-title">Testing</div>
+                    <div class="board-column-content">
+                    </div>
+                </div>
+                <div class="board-column">
+                    <div class="board-column-title">Done</div>
+                    <div class="board-column-content">
+                    </div>
+                </div>
+            </div>
+        </section>
             </div>`;
 }
 
